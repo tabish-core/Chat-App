b7d948843d24a2d4b3f720ed3c4544278129535e
tabish-core <tabish.core@gmail.com>
2026-02-19 17:12:34 +0500

    Login, Logout & Update Profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -14,6 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 

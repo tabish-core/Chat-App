efd56f09fc36cc19ed2cdc4c85319c2a64aae021
tabish-core <tabish.core@gmail.com>
2026-02-19 16:02:29 +0500

    db Schema & Signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,15 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5001;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () =>{
     console.log("Server is running on PORT:" + PORT);

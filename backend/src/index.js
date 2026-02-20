fa218f45304ba3bfbcb0d11d2cc2fdea481cf2eb
tabish-core <tabish.core@gmail.com>
2026-02-20 14:50:53 +0500

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 
 dotenv.config();
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(PORT, () =>{

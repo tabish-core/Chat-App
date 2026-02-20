39342570c21b00d0f83756a9acd59f827ab377b8
tabish-core <tabish.core@gmail.com>
2026-02-20 16:00:18 +0500

    CORS Policy import fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -18,6 +19,11 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);

9b73d2a8629dbd3f75386fbc02d32cbbd0e0584f
tabish-core <tabish.core@gmail.com>
2026-02-23 15:09:09 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,10 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 5001;
 
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-app.listen(PORT, () =>{
+server.listen(PORT, () =>{
     console.log("Server is running on PORT:" + PORT);
     connectDB();
 });

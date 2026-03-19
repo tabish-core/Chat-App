dfd75bf70c8f0ba0785a77fab6581542b0136b2c
tabish-core <tabish.core@gmail.com>
2026-03-19 11:37:26 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
 
 }));

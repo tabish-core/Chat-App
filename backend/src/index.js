d531fdd979626bdb433c2f588cd04debd4c73c1b
tabish-core <tabish.core@gmail.com>
2026-03-19 11:41:47 +0500

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import dns from "node:dns/promises";
-dns.setServers(["1.1.1.1", "8.8.8.8"]); 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -16,6 +17,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
@@ -29,8 +31,16 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-server.listen(PORT, () =>{
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+
+server.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
     connectDB();
 });
